c8388d5bf973c4d49fd3c26f45665ed22870118e
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-12 12:24:54 +0000

    fix: countryCode build

--- app/composables/user-country.ts
+++ app/composables/user-country.ts
@@ -1,6 +1,10 @@
 export const useUserCountry = () => {
+  const route = useRoute()
+
   const regionIdFromCookie = useCookie('user_region')
   const countryCodeFromCookie = useCookie('user_country')
+
+  const countryCodeFromRoute = computed(() => route.params.countryCode as string)
 
   const setCountry = (country: BaseRegionCountryWithRegionId) => {
     regionIdFromCookie.value = country.region_id
@@ -9,7 +13,7 @@ export const useUserCountry = () => {
 
   return {
     userRegionId: computed(() => regionIdFromCookie.value || undefined),
-    userCountryCode: computed(() => countryCodeFromCookie.value || undefined),
+    userCountryCode: computed(() => countryCodeFromCookie.value || countryCodeFromRoute.value || undefined),
     setCountry,
   }
 }
