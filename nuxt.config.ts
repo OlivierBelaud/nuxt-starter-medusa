36c1ff60495d945fcf81ef8ec57d077ff0402696
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-18 12:11:42 +0000

    feat: finetune prerender route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,9 +51,19 @@ export default defineNuxtConfig({
           'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
         },
       }).then(res => res.json())
+      const { products } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/products`, {
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
+        },
+      }).then(res => res.json())
       const countries = regions?.map((region: StoreRegion) => region.countries).flat()
       for (const country of countries) {
         ctx.routes.add(`/${country.iso_2.toLowerCase()}`)
+        for (const product of products) {
+          ctx.routes.add(`/${country.iso_2.toLowerCase()}/products/${product.handle.toLowerCase()}`)
+        }
       }
     },
   },
