8bd91a9a26b53451bd3a9bd5369589947393d833
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-19 12:57:56 +0000

    chore: remove nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,6 @@ export default defineNuxtConfig({
     '@nuxtjs/medusa',
     '@nuxt/ui',
     '@nuxt/image',
-    '@nuxthub/core',
     '@nuxt/eslint',
   ],
   devtools: { enabled: true },
@@ -20,10 +19,6 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
   },
-
-  // routeRules: {
-  //   '/fr/products/pinnacle-posh-pack': { prerender: true },
-  // },
 
   future: {
     compatibilityVersion: 4,
@@ -40,21 +35,8 @@ export default defineNuxtConfig({
     prerender: {
       crawlLinks: true,
     },
-    cloudflare: {
-      pages: {
-        routes: {
-          exclude: [
-            '/*',
-          ],
-        },
-      },
-    },
   },
 
-  // https://hub.nuxt.com/docs/getting-started/installation#options
-  // hub: {
-  //   cache: true,
-  // },
   hooks: {
     async 'prerender:routes'(ctx) {
       const { regions } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/regions`, {
@@ -64,24 +46,20 @@ export default defineNuxtConfig({
           'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
         },
       }).then(res => res.json())
-      const { products } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/products`, {
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
-        },
-      }).then(res => res.json())
+      // const { products } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/products`, {
+      //   credentials: 'include',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
+      //   },
+      // }).then(res => res.json())
       const countries = regions?.map((region: StoreRegion) => region.countries).flat()
       for (const country of countries) {
         ctx.routes.add(`/${country.iso_2.toLowerCase()}`)
-        for (const product of products) {
-          ctx.routes.add(`/${country.iso_2.toLowerCase()}/products/${product.handle.toLowerCase()}`)
-        }
+        // for (const product of products) {
+        //   ctx.routes.add(`/${country.iso_2.toLowerCase()}/products/${product.handle.toLowerCase()}`)
+        // }
       }
-      // ctx.routes.add(`/fr`)
-      // for (const product of products) {
-      //   ctx.routes.add(`/fr/products/${product.handle.toLowerCase()}`)
-      // }
     },
   },
 
