91d0110320bcd5bfb724e186d132dbddb5ff02cd
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-20 11:05:20 +0000

    refactor: useFilter with computed

--- app/composables/filters.ts
+++ app/composables/filters.ts
@@ -19,49 +19,33 @@ export const useFilters = () => {
     },
   ]
 
-  const pageNumber = ref(route.query.page ? parseInt(route.query.page as string) : 1)
-  const sortBy = ref((route.query.sortBy as SortOptionsType | undefined) || SORT_OPTIONS.CREATED_AT)
-
-  // watch(route, () => {
-  //   console.log('route.query.page', route.query.page)
-  // })
-
-  // watchEffect(() => {
-  //   console.log('watchEffect')
-  //   pageNumber.value = route.query.page ? parseInt(route.query.page as string) : 1
-  //   console.log('pageNumber', pageNumber.value)
-  // })
-
-  // onMounted(() => {
-  //   pageNumber.value = route.query.page ? parseInt(route.query.page as string) : 1
-  //   sortBy.value = (route.query.sortBy as SortOptionsType | undefined) || SORT_OPTIONS.CREATED_AT
-
-  //   console.log('onMounted', pageNumber.value)
-  // })
-
-  watch(pageNumber, (newPageNumber) => {
-    const newQuery = { ...route.query }
-    delete newQuery.page
-    if (newPageNumber !== 1) {
-      newQuery.page = newPageNumber.toString()
-    }
-    router.push({
-      query: newQuery,
-    })
+  const pageNumber = computed<number>({
+    get: () => route.query.page ? parseInt(route.query.page as string) : 1,
+    set: (newPage: number) => {
+      const newQuery = { ...route.query }
+      if (newPage === 1) {
+        delete newQuery.page
+      }
+      else {
+        newQuery.page = newPage.toString()
+      }
+      router.push({ query: newQuery })
+    },
   })
 
-  watch(sortBy, async (newSortBy) => {
-    const newQuery = { ...route.query }
-    delete newQuery.sortBy
-    if (newSortBy !== SORT_OPTIONS.CREATED_AT) {
-      newQuery.sortBy = newSortBy
-    }
-    await router.push({
-      query: newQuery,
-    })
-    nextTick(() => {
+  const sortBy = computed<SortOptionsType>({
+    get: () => (route.query.sortBy as SortOptionsType) || SORT_OPTIONS.CREATED_AT,
+    set: async (newSortBy: SortOptionsType) => {
+      const newQuery = { ...route.query }
+      if (newSortBy === SORT_OPTIONS.CREATED_AT) {
+        delete newQuery.sortBy
+      }
+      else {
+        newQuery.sortBy = newSortBy
+      }
+      await router.push({ query: newQuery })
       pageNumber.value = 1
-    })
+    },
   })
 
   return {
