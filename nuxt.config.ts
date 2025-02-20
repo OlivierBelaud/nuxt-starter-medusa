860d36a08285061f5bb9254b88dd304963cd53a0
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-20 10:43:56 +0000

    feat: product list as server component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,10 +23,10 @@ export default defineNuxtConfig({
   routeRules: {
     '/**/': { prerender: true },
     '/**/products/**': { prerender: true },
-    '/**/collections/**': { isr: true },
-    '/**/categories/**': { isr: true },
+    '/**/collections/**': { prerender: true },
+    '/**/categories/**': { prerender: true },
     '/**/account': { prerender: true },
-    '/**/store': { isr: true },
+    '/**/store': { prerender: true },
     '/**/cart': { prerender: true },
     '/**/checkout': { prerender: true },
   },
@@ -48,10 +48,10 @@ export default defineNuxtConfig({
         routes: {
           exclude: [
             '/**/products/**',
-            // '/**/collections/**',
-            // '/**/categories/**',
+            '/**/collections/**',
+            '/**/categories/**',
             '/**/account',
-            // '/**/store',
+            '/**/store',
             '/**/cart',
             '/**/checkout',
           ],
@@ -80,36 +80,36 @@ export default defineNuxtConfig({
           'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
         },
       }).then(res => res.json())
-      // const { collections } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/collections`, {
-      //   credentials: 'include',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
-      //   },
-      // }).then(res => res.json())
-      // const { product_categories: categories } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/product-categories`, {
-      //   credentials: 'include',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
-      //   },
-      // }).then(res => res.json())
+      const { collections } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/collections`, {
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
+        },
+      }).then(res => res.json())
+      const { product_categories: categories } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/product-categories`, {
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
+        },
+      }).then(res => res.json())
       const countries = regions?.map((region: StoreRegion) => region.countries).flat()
       for (const country of countries) {
         ctx.routes.add(`/${country.iso_2}`)
         ctx.routes.add(`/${country.iso_2}/account`)
-        // ctx.routes.add(`/${country.iso_2}/store`)
+        ctx.routes.add(`/${country.iso_2}/store`)
         ctx.routes.add(`/${country.iso_2}/cart`)
         ctx.routes.add(`/${country.iso_2}/checkout`)
         for (const product of products) {
           ctx.routes.add(`/${country.iso_2}/products/${product.handle}`)
         }
-        // for (const collection of collections) {
-        //   ctx.routes.add(`/${country.iso_2}/collections/${collection.handle}`)
-        // }
-        // for (const category of categories) {
-        //   ctx.routes.add(`/${country.iso_2}/categories/${category.handle}`)
-        // }
+        for (const collection of collections) {
+          ctx.routes.add(`/${country.iso_2}/collections/${collection.handle}`)
+        }
+        for (const category of categories) {
+          ctx.routes.add(`/${country.iso_2}/categories/${category.handle}`)
+        }
       }
     },
   },
