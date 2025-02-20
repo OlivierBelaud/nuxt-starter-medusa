5e987c5578616879f2a4b0a0dc5044d3fe535863
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-20 08:15:24 +0000

    chore: activate swr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import type { StoreRegion } from '@medusajs/types'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
@@ -17,21 +19,17 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   runtimeConfig: {
-    public: {
-      medusaBackendUrl: process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL,
-      medusaPublishableKey: process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
-    },
   },
-  // routeRules: {
-  //   '/**/': { swr: true },
-  //   '/**/products/**': { prerender: true },
-  //   '/**/collections/**': { swr: true },
-  //   '/**/categories/**': { swr: true },
-  //   '/**/account': { prerender: true },
-  //   '/**/store': { prerender: true },
-  //   '/**/cart': { prerender: true },
-  //   '/**/checkout': { prerender: true },
-  // },
+  routeRules: {
+    '/**/': { swr: true },
+    '/**/products/**': { swr: true },
+    '/**/collections/**': { swr: true },
+    '/**/categories/**': { swr: true },
+    '/**/account': { prerender: true },
+    '/**/store': { prerender: true },
+    '/**/cart': { prerender: true },
+    '/**/checkout': { prerender: true },
+  },
 
   future: {
     compatibilityVersion: 4,
@@ -44,30 +42,77 @@ export default defineNuxtConfig({
     // },
   },
   compatibilityDate: '2024-11-06',
-  nitro: {
-    // Use pattern to avoid 100 the Cloudflare 100 routes limit
-    // https://hub.nuxt.com/docs/recipes/pre-rendering#cloudflare-100-routes-limit
-    cloudflare: {
-      pages: {
-        routes: {
-          exclude: [
-            '/**/products/**',
-            '/**/collections/**',
-            '/**/categories/**',
-            '/**/account',
-            '/**/store',
-            '/**/cart',
-            '/**/checkout',
-          ],
-        },
-      },
-    },
-  },
+  // nitro: {
+  //   cloudflare: {
+  //     pages: {
+  //       routes: {
+  //         exclude: [
+  //           '/**/products/**',
+  //           '/**/collections/**',
+  //           '/**/categories/**',
+  //           '/**/account',
+  //           '/**/store',
+  //           '/**/cart',
+  //           '/**/checkout',
+  //         ],
+  //       },
+  //     },
+  //   },
+  // },
 
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
     cache: true,
   },
+  // hooks: {
+  //   async 'prerender:routes'(ctx) {
+  //     const { regions } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/regions`, {
+  //       credentials: 'include',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
+  //       },
+  //     }).then(res => res.json())
+  //     const { products } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/products`, {
+  //       credentials: 'include',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
+  //       },
+  //     }).then(res => res.json())
+  //     const { collections } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/collections`, {
+  //       credentials: 'include',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
+  //       },
+  //     }).then(res => res.json())
+  //     const { product_categories: categories } = await fetch(`${process.env.NUXT_PUBLIC_MEDUSA_BACKEND_URL}/store/product-categories`, {
+  //       credentials: 'include',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'x-publishable-api-key': process.env.NUXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY || '',
+  //       },
+  //     }).then(res => res.json())
+  //     const countries = regions?.map((region: StoreRegion) => region.countries).flat()
+  //     for (const country of countries) {
+  //       ctx.routes.add(`/${country.iso_2}`)
+  //       ctx.routes.add(`/${country.iso_2}/account`)
+  //       ctx.routes.add(`/${country.iso_2}/store`)
+  //       ctx.routes.add(`/${country.iso_2}/cart`)
+  //       ctx.routes.add(`/${country.iso_2}/checkout`)
+  //       for (const product of products) {
+  //         ctx.routes.add(`/${country.iso_2}/products/${product.handle}`)
+  //       }
+  //       for (const collection of collections) {
+  //         ctx.routes.add(`/${country.iso_2}/collections/${collection.handle}`)
+  //       }
+  //       for (const category of categories) {
+  //         ctx.routes.add(`/${country.iso_2}/categories/${category.handle}`)
+  //       }
+  //     }
+  //   },
+  // },
 
   eslint: {
     config: {
