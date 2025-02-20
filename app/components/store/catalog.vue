fdb74c56e9149c47ad4dd53d3b0a79c63f45c0df
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-20 11:13:00 +0000

    refactor: readd refresh

--- app/components/store/catalog.vue
+++ app/components/store/catalog.vue
@@ -33,7 +33,7 @@ watchEffect(() => {
   console.log('pageNumber', pageNumber.value)
 })
 
-const { data } = await useFetchProductsWithCache({
+const { data, refresh } = await useFetchProductsWithCache({
   query: productsQuery,
 })
 
@@ -42,10 +42,9 @@ const count = computed(() => data.value?.count || 0)
 
 const displayPagination = computed(() => count.value > defaultProductsPerPage)
 
-// onMounted(() => {
-//   console.log('onMounted')
-//   refresh()
-// })
+onMounted(() => {
+  refresh()
+})
 </script>
 
 <template>
