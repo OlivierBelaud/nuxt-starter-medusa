bd6b2d5dd125254388d3f875be640bb38261453d
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-26 16:33:01 +0000

    perf: add other console 2

--- app/pages/[countryCode]/products/[handle].vue
+++ app/pages/[countryCode]/products/[handle].vue
@@ -6,6 +6,7 @@ const { data, isStatic, refresh } = await useFetchProductByHandle(handle.value)
 const product = computed(() => data.value?.products[0])
 
 onMounted(() => {
+  console.log('Product page mounted')
   refresh()
 })
 </script>
