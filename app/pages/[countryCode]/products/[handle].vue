6e03ebf79f84d7dfbf1bd77df30be71ac6c055f0
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-27 16:19:20 +0000

    feat: add refreshCachedData for each product page load

--- app/pages/[countryCode]/products/[handle].vue
+++ app/pages/[countryCode]/products/[handle].vue
@@ -2,14 +2,15 @@
 const route = useRoute()
 
 const handle = computed(() => route.params.handle as string)
-const { data, origin, refreshCachedData } = await useFetchProductByHandle(handle.value)
+const { data, origin, refreshCachedData } = useFetchProductByHandle(handle.value)
 const product = computed(() => data.value?.products[0])
 const isStatic = computed(() => origin.value.fetchOrigin === 'static')
 
-// onMounted(() => {
-//   console.log('Product page mounted')
-//   refreshCachedData()
-// })
+onMounted(() => {
+  // here i want to have the freshest data possible, so I will tel the browser each time.
+  console.log('Product page mounted')
+  refreshCachedData()
+})
 </script>
 
 <template>
