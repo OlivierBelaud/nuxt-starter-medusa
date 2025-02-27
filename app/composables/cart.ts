5d8ee8d76f14477bb5203f0e0a20df48c3dc7f9d
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-27 10:27:13 +0000

    perf: cart key

--- app/composables/cart.ts
+++ app/composables/cart.ts
@@ -35,7 +35,7 @@ export const useFetchCart = () => {
   const { retrieveCart } = useCart()
 
   return useLazyAsyncData(
-    'cart',
+    'cart-exp',
     async () => await retrieveCart(),
     {
       // server: false,
