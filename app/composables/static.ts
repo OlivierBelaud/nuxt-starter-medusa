1cd33a22dcb88f259377db09f95ed57570689d4b
OlivierBelaud <olivierbelaudpro@gmail.com>
2025-02-27 12:08:23 +0000

    perf: cart empty request 7

--- app/composables/static.ts
+++ app/composables/static.ts
@@ -1,45 +1,39 @@
 import type { AsyncDataOptions, NuxtApp } from '#app'
 
+interface Context {
+  fetchOrigin: 'server' | 'client' | 'static'
+  fetchTimestamp: number
+}
+
 export const useStaticAsyncData = <T>(
   key: string,
   fetcher: (nuxtApp?: NuxtApp) => Promise<T>,
   options?: AsyncDataOptions<T>,
 ) => {
   const nuxtApp = useNuxtApp()
-  const isStatic = useState<boolean>(`isStatic-${key}`, () => !!nuxtApp.payload.prerenderedAt)
 
-  const origin = useState<string>(`origin-${key}`, () => nuxtApp.payload.prerenderedAt ? 'static' : 'server')
-  // const isServer = useState<boolean>(`isStatic-${key}`, () => import.meta.server)
-  // const isClient = useState<boolean>(`isStatic-${key}`, () => import.meta.client)
-  // const origin = ref()
-  const time = ref()
+  const origin = useState<Context>(`context-${key}`, () => ({
+    fetchOrigin: nuxtApp.payload.prerenderedAt ? 'static' : 'server',
+    fetchTimestamp: nuxtApp.payload.prerenderedAt || Date.now(),
+  }))
 
-  const { data, status, error } = useLazyAsyncData<T>(
+  const { data, status, error, execute, clear } = useLazyAsyncData<T>(
     key,
     () => {
       const callTimestamp = Date.now()
-      // if (isStatic.value) {
-      //   origin.value = 'static'
-      //   time.value = nuxtApp.payload.prerenderedAt
-      // }
-      // else if (import.meta.server) {
-      //   origin.value = 'server'
-      // }
-      // else {
-      //   origin.value = 'client'
-      // }
       if (import.meta.server) {
-        origin.value = 'server'
+        origin.value = {
+          fetchOrigin: 'server',
+          fetchTimestamp: callTimestamp,
+        }
       }
       else if (import.meta.client) {
-        origin.value = 'client'
+        console.log('fetching data from client')
+        origin.value = {
+          fetchOrigin: 'client',
+          fetchTimestamp: callTimestamp,
+        }
       }
-      time.value = callTimestamp
-      // console.log('callTimestamp', key, callTimestamp)
-      // console.log('import.meta', key, import.meta)
-      // console.log('isServer', key, import.meta.server)
-      // console.log('isClient', key, import.meta.client)
-      // console.log('origin.value', origin.value, time.value)
       console.log('fetching data from', import.meta.server ? 'server' : 'client')
       return fetcher()
     },
@@ -47,24 +41,20 @@ export const useStaticAsyncData = <T>(
   )
 
   watchEffect(() => {
-    console.log('origin.value', origin.value, time.value)
-    // console.log('isStatic', key, isStatic.value)
-    // // console.log('isServer', key, isServer.value)
-    // // console.log('isClient', key, isClient.value)
-    // console.log('import.meta', key, import.meta)
-    // console.log('nuxtApp.payload', nuxtApp.payload)
+    console.log('origin.value', origin.value)
   })
 
   const refresh = async (): Promise<void> => {
     await refreshNuxtData(key)
-    isStatic.value = false
   }
 
   return {
     data,
     status,
     error,
-    isStatic,
+    origin,
     refresh,
+    execute,
+    clear,
   }
 }
--- app/pages/[countryCode]/products/[handle].vue
+++ app/pages/[countryCode]/products/[handle].vue
@@ -2,13 +2,14 @@
 const route = useRoute()
 
 const handle = computed(() => route.params.handle as string)
-const { data, isStatic, refresh } = await useFetchProductByHandle(handle.value)
+const { data, origin, refresh } = await useFetchProductByHandle(handle.value)
 const product = computed(() => data.value?.products[0])
+const isStatic = computed(() => origin.value.fetchOrigin === 'static')
 
-// onMounted(() => {
-//   console.log('Product page mounted')
-//   refresh()
-// })
+onMounted(() => {
+  console.log('Product page mounted')
+  refresh()
+})
 </script>
 
 <template>
